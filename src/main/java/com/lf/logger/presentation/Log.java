9f5c356a22d3d1114bffa3c0f8ea341935ce41f3
LorenzoFritzsch <lorenzo.fritzsch@gmail.com>
2022-04-11 23:52:59 +0200

    Change Log to static methods

--- src/main/java/com/lf/logger/presentation/Log.java
+++ src/main/java/com/lf/logger/presentation/Log.java
@@ -1,32 +1,27 @@
 package com.lf.logger.presentation;
 
 import com.lf.logger.data.repositories.LoggerRepositoryImpl;
-import com.lf.logger.domain.repositories.LoggerRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Log implements LoggerRepository {
+public class Log{
 
-    ApplicationContext context = new AnnotationConfigApplicationContext(Log.class);
-    LoggerRepositoryImpl loggerRepository = context.getBean(LoggerRepositoryImpl.class);
+    static ApplicationContext context = new AnnotationConfigApplicationContext(Log.class);
+    static LoggerRepositoryImpl loggerRepository = context.getBean(LoggerRepositoryImpl.class);
 
-    @Override
-    public void e(String UTCTime, String machineName, String tag, String context, String message) {
+    public static void e(String UTCTime, String machineName, String tag, String context, String message) {
         loggerRepository.e(UTCTime, machineName, tag, context, message);
     }
-    
-    @Override
-    public void i(String UTCTime, String machineName, String tag, String context, String message) {
+
+    public static void i(String UTCTime, String machineName, String tag, String context, String message) {
         loggerRepository.i(UTCTime, machineName, tag, context, message);
     }
 
-    @Override
-    public void w(String UTCTime, String machineName, String tag, String context, String message) {
+    public static void w(String UTCTime, String machineName, String tag, String context, String message) {
         loggerRepository.w(UTCTime, machineName, tag, context, message);
     }
 
-    @Override
-    public void d(String UTCTime, String machineName, String tag, String context, String message) {
+    public static void d(String UTCTime, String machineName, String tag, String context, String message) {
         loggerRepository.d(UTCTime, machineName, tag, context, message);
     }
 }
