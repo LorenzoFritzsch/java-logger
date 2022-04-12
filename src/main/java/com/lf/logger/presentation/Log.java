ae4c3e9865f01c5f8e396711ae1a517bb436dff7
LorenzoFritzsch <lorenzo.fritzsch@gmail.com>
2022-04-12 11:17:10 +0200

    Remove UTCTime as input

--- src/main/java/com/lf/logger/presentation/Log.java
+++ src/main/java/com/lf/logger/presentation/Log.java
@@ -4,24 +4,30 @@ import com.lf.logger.data.repositories.LoggerRepositoryImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.time.Instant;
+
 public class Log{
 
     static ApplicationContext context = new AnnotationConfigApplicationContext(Log.class);
     static LoggerRepositoryImpl loggerRepository = context.getBean(LoggerRepositoryImpl.class);
 
-    public static void e(String UTCTime, String tag, String context, String message) {
+    public static void e(String tag, String context, String message) {
+        String UTCTime = Instant.now().toString();
         loggerRepository.e(UTCTime, tag, context, message);
     }
 
-    public static void i(String UTCTime, String tag, String context, String message) {
+    public static void i(String tag, String context, String message) {
+        String UTCTime = Instant.now().toString();
         loggerRepository.i(UTCTime, tag, context, message);
     }
 
-    public static void w(String UTCTime, String tag, String context, String message) {
+    public static void w(String tag, String context, String message) {
+        String UTCTime = Instant.now().toString();
         loggerRepository.w(UTCTime, tag, context, message);
     }
 
-    public static void d(String UTCTime, String tag, String context, String message) {
+    public static void d(String tag, String context, String message) {
+        String UTCTime = Instant.now().toString();
         loggerRepository.d(UTCTime, tag, context, message);
     }
 }
